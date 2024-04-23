7b60dcb6d09ce66173f28ff3d325ec810371e1d2
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-04-23 14:18:30 +0200

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/helper CalcoliHelper.cs
@@ -0,0 +1,47 @@
+﻿namespace csharp_calcolatrice
+{
+    internal static class CalcoliHelper
+    {
+        public static int Somma(int numero_uno , int numero_due)
+        {
+            return numero_uno + numero_due;
+        }
+
+        public static double Somma(double numero_uno, double numero_due)
+        {
+            return numero_uno + numero_due;
+        }
+
+        public static int Differenza(int numero_uno, int numero_due)
+        {
+            return numero_uno - numero_due;
+        }
+
+        public static double Differenza(double numero_uno, double numero_due)
+        {
+            return numero_uno - numero_due;
+        }
+
+        public static int Moltiplicazione(int numero_uno, int numero_due)
+        {
+            return numero_uno * numero_due;
+        }
+
+        public static double Moltiplicazione(double numero_uno, double numero_due)
+        {
+            return numero_uno * numero_due;
+        }
+
+        //public static int ValoreAssoluto(int numero_uno, int numero_due)
+        //{
+            //if (numero_uno >= 0) 
+            //{
+                //return a;
+            //}
+            //else
+            //{
+
+            //}
+        //}
+    }
+}
